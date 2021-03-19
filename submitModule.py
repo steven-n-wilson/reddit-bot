fd07499ef83de25e9450f97fa3ff68768058b20b
steven-n-wilson <49218333+steven-n-wilson@users.noreply.github.com>
2021-03-18 21:29:15 -0600

    improved selftext string

--- submitModule.py
+++ submitModule.py
@@ -3,17 +3,16 @@ from praw.models import InlineImage
 
 def build_template(articles, meme):
 
-    meme_path = meme[0]
-    meme_caption = meme[1]
-
-    image = InlineImage(meme_path, meme_caption)
+    image = InlineImage(path=meme[0], caption=meme[1])
     media = {"image1": image}
 
     title = "📍 Markdown template"
-    selftext = """Hello 👋 Today's botly news articles are:
-* [First news article](https://markdownlivepreview.com/) Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae scelerisque ipsum. Duis imperdiet arcu nulla, quis rhoncus risus porttitor nec. Etiam sit amet posuere diam.  
-* [Second news article](https://markdownlivepreview.com/) Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae scelerisque ipsum. Duis imperdiet arcu nulla, quis rhoncus risus porttitor nec. Etiam sit amet posuere diam.  
-* [Third news article](https://markdownlivepreview.com/) Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae scelerisque ipsum. Duis imperdiet arcu nulla, quis rhoncus risus porttitor nec. Etiam sit amet posuere diam.  
-{image1}
-    """
+
+    article1 = f"\n * [{articles[0]}]({articles[1]}) {articles[2]}"
+    article2 = "\n * [Second news article](https://markdownlivepreview.com/) Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae scelerisque ipsum. Duis imperdiet arcu nulla, quis rhoncus risus porttitor nec. Etiam sit amet posuere diam."
+    article3 = "\n * [Third news article](https://markdownlivepreview.com/) Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae scelerisque ipsum. Duis imperdiet arcu nulla, quis rhoncus risus porttitor nec. Etiam sit amet posuere diam."
+
+    selftext = "Hello 👋 Today's botly news articles are:" + \
+        article1 + article2 + article3 + "{image1}"
+
     return [title, selftext, media]
