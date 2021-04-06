9679737c9683dfab8f48680bd47db72a21ffa19b
steven-n-wilson <49218333+steven-n-wilson@users.noreply.github.com>
2021-04-06 17:41:46 -0600

    added mit func

--- articlesModule.py
+++ articlesModule.py
@@ -1,15 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
 
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9'}
+
 
 def get_nytimes_article():
 
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9'}
     response = requests.get(
         "https://www.nytimes.com/section/technology", headers=headers)
 
-    print("Response: ", response)
+    print("Response from NYT: ", response)
 
     soup = BeautifulSoup(response.content, 'lxml')
 
@@ -21,4 +22,39 @@ def get_nytimes_article():
     return [article_title, article_url, article_description]
 
 
-# get_nytimes_article()
+def get_wired_article():
+
+    response = requests.get(
+        "https://www.wired.com/category/science/", headers=headers)
+
+    print("Response from Wired: ", response)
+
+    soup = BeautifulSoup(response.content, 'lxml')
+
+    article_info = soup.find_all(
+        "a", attrs={"class": "post-listing-list-item__link"})
+
+    article_url = "https://www.wired.com" + article_info[8].get('href')
+    article_title = article_info[8].find('h5').get_text()
+    article_description = ""
+
+    return [article_title, article_url, article_description]
+
+
+def get_mit_article():
+
+    response = requests.get(
+        "https://www.technologyreview.com/", headers=headers)
+
+    print("Response from MIT: ", response)
+
+    soup = BeautifulSoup(response.content, 'lxml')
+
+    article_info = soup.find(
+        "a", attrs={"class": "popular__itemTitle--3oYx0"})
+
+    article_url = article_info.get('href')
+    article_title = article_info.get_text()
+    article_description = ""
+
+    return [article_title, article_url, article_description]
